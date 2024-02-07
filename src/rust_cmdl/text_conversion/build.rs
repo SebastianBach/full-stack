0af2f10fa7664c46eb7086b7f721b7431cb4f0ab
Sebastian Bach <bachsebastian@gmx.de>
2024-02-07 20:09:38 +0100

    cleanup

--- src/rust_cmdl/text_conversion/build.rs
+++ src/rust_cmdl/text_conversion/build.rs
@@ -1,16 +1,35 @@
+use std::path::Path;
 
-fn main() {
-
+fn setup() -> Result<(), &'static str> {
+    
     match std::env::var("C_WRAPPER_LIB_LOCATION") {
         Ok(lib_location) => {
+
             println!("C_WRAPPER_LIB_LOCATION is: {}", lib_location);
-            println!("cargo:rustc-link-search=native={}", lib_location);
+            let path = Path::new(&lib_location);
+
+            if path.exists() && path.is_dir() {
+                println!("cargo:rustc-link-search=native={}", lib_location);
+            } else {
+                return Err("C_WRAPPER_LIB_LOCATION is not an existing directory.");
+            }
         },
-        Err(e) => println!("Couldn't read C_WRAPPER_LIB_LOCATION ({})", e),
+        Err(_) => {
+            return Err("Couldn't read C_WRAPPER_LIB_LOCATION.");
+        },
     }
 
     println!("cargo:rustc-link-lib=static=text_conversion_c");
-
     println!("cargo:rustc-link-lib=stdc++");
     println!("cargo:rustc-link-lib=c");
+
+    Ok(())
+}
+
+fn main() {
+
+    if let Err(e) = setup()
+    {
+        panic!("Build setup failed: {}", e);
+    } 
 }
--- src/rust_cmdl/text_conversion/src/main.rs
+++ src/rust_cmdl/text_conversion/src/main.rs
@@ -1,3 +1,5 @@
+// https://doc.rust-lang.org/nomicon/ffi.html
+
 extern crate libc;
 use libc::{c_char, size_t};
 use std::ffi::CString;
@@ -15,18 +17,17 @@ fn perform_text_conversion(input: &str) -> String {
   let mut buffer: Vec<u8> = c_string.into_bytes_with_nul();
 
   unsafe {
-  
     text_conversion_c(buffer.as_mut_ptr() as *mut c_char, buffer.len());
-
-    // check for null terminator
-    if buffer[buffer.len() - 1] != 0 {
-      buffer.push(0);
-    }
-
-    // convert back to string
-    let string = String::from_utf8_lossy(&buffer).to_string();
-    return string;
   }
+
+  // check for null terminator
+  if buffer[buffer.len() - 1] != 0 {
+    buffer.push(0);
+  }
+
+  // convert back to string
+  let string = String::from_utf8_lossy(&buffer).to_string();
+  return string;
 }
 
 fn main() {
@@ -42,11 +43,11 @@ fn main() {
 
   let input_string: &str = &args[1];
 
-  println!("Input:  {}", input_string);
+  println!("Input: \"{}\"", input_string);
 
   let result = perform_text_conversion(input_string);
 
-  println!("Result: {}", result);
+  println!("Result: \"{}\"", result);
 
   process::exit(0);
 }
