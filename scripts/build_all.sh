301fbfeb2996a4a31b84a2e848299af73f26da7b
Sebastian Bach <bachsebastian@gmx.de>
2024-03-29 17:17:50 +0100

    build all script

---
+++ scripts/build_all.sh
@@ -0,0 +1,17 @@
+echo build all
+
+trap 'echo "Error: Command failed"; exit 1' ERR
+
+cd ..
+
+if [ ! -d "build" ]; then
+    echo "make build folder"
+    mkdir build
+fi
+
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DADD_PYTHON_MODULE=ON -DADD_QT_APP=ON -DADD_RUST_APP=ON -DADD_PY_DOCS=ON -DADD_LIB_DOCS=ON -DADD_JAVA_APP=ON -DADD_SCRIPT_TOOLS=ON -DADD_ASSEMBLY_PROGRAM=ON ..
+cmake --build . -j --config Release
+ctest -C Release  -VV
+cmake --install .
