0a7db82e0bbfaea29b2354e3b170bfaf32003305
SebastianBach <bachsebastian@gmx.de>
2024-04-06 00:33:23 +0200

    cleanup

--- src/app_android/app/src/main/cpp/native-lib.cpp
+++ src/app_android/app/src/main/cpp/native-lib.cpp
@@ -2,21 +2,10 @@
 #include <string>
 #include "c_wrapper.h"
 
-
 extern "C" JNIEXPORT
 
 jstring JNICALL
-Java_com_example_titlecase_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "brave new world";
-    return env->NewStringUTF(hello.c_str());
-}
-
-extern "C" JNIEXPORT
-
-jstring JNICALL
-Java_com_example_titlecase_MainActivity_editFromJNI(
+Java_com_example_titlecase_MainActivity_titleCase(
         JNIEnv *env,
         jobject /* this */, jstring input) {
 
@@ -25,7 +14,6 @@ Java_com_example_titlecase_MainActivity_editFromJNI(
     env->ReleaseStringUTFChars(input, cStr);
 
     text_conversion_c(cppStr.data(), cppStr.length());
-
 
     return env->NewStringUTF(cppStr.c_str());
 }
