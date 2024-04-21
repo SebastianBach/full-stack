2ffad3ffbef71b64aa56b0a4f7527ec2ebee1b79
SebastianBach <bachsebastian@gmx.de>
2024-04-21 11:39:14 +0200

    extended valgrind check

--- scripts/build_valgrind.sh
+++ scripts/build_valgrind.sh
@@ -2,8 +2,27 @@ cd ..
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DADD_BASIC_TOOLS=ON -DBUILD_VALGRIND=ON .. 
+cmake           -DCMAKE_BUILD_TYPE=Debug\
+                -DBUILD_VALGRIND=ON\
+                -DADD_BASIC_TOOLS=ON\
+                -DADD_SDK=ON\
+                -DADD_C_LIB=ON\
+                -DADD_DYNAMIC_LIB=ON\
+                -DADD_QT_APP=ON\
+                -DADD_RUST_APP=ON\
+                -DADD_SCRIPT_TOOLS=ON\
+                -DADD_ASSEMBLY_PROGRAM=ON ..
+
 cmake --build .
-#valgrind --leak-check=full ctest
 ctest -T memcheck 
-cat ./Testing/Temporary/MemoryChecker.*.log
+
+cd ./Testing/Temporary/
+
+for file in MemoryChecker.*.log; do
+    if grep -q "ERROR SUMMARY" "$file"; then
+        if ! grep -q "ERROR SUMMARY: 0 errors from 0 contexts" "$file"; then
+            echo "File: $file contains errors"
+            cat "$file"
+        fi
+    fi
+done
